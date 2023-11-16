2a1bcb6fe276efd319101b5548b35f4324b357b7
Amir Mosquera <amamosni@gmail.com>
2023-11-16 15:59:24 -0500

    Commit para agregar a GitHub

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 const h1 = document.querySelector('h1')
+const form = document.getElementById('formulario')
 const input1 = document.getElementById('calculo1')
 const input2 = document.getElementById('calculo2')
 const btn = document.getElementById('btnCalcular')
 const result = document.getElementById('resultado')
 
-function btnOnClick() {
+form.addEventListener('submit', sumarInputValues)
+
+function sumarInputValues(event) {
+    //console.log({event});
+    event.preventDefault()
     sumaInput = parseFloat(input1.value) + Number(input2.value)
     result.innerText = "Resultado: " + sumaInput
 }
