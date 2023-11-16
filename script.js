74484aea6dd6e5321a496996bf8ec7bb9ec91d57
Amir Mosquera <amamosni@gmail.com>
2023-11-16 07:52:08 -0500

    Escuchar eventos desde el HTML

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log("Hello, world")
+const h1 = document.querySelector('h1')
+const input1 = document.getElementById('calculo1')
+const input2 = document.getElementById('calculo2')
+const btn = document.getElementById('btnCalcular')
+const result = document.getElementById('resultado')
+
+function btnOnClick() {
+    sumaInput = parseFloat(input1.value) + Number(input2.value)
+    result.innerText = "Resultado: " + sumaInput
+}
